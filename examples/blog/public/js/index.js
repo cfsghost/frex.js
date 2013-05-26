c4e7dd824b118caf11eda710ba96690c803bc4e5
Fred Chien <fred@mandice.com>
2013-05-26 20:59:23 +0800

    support setter of property

--- examples/blog/public/js/index.js
+++ examples/blog/public/js/index.js
@@ -2,10 +2,11 @@
 App.require('Blog', function() {
 	var blog = App.Engine('Blog');
 
-	console.log('===== ' + blog.title);
-	window.title = blog.title;
-
 	blog.listArticles({}, function(err, articles) {
 		console.log(articles);
 	});
+
+	blog.title = 'Test';
+
+	console.log(blog.title);
 });
