ba8c2f6778ec7027fc5a85ae216fa4beed6bb374
Fred Chien <fred@mandice.com>
2013-05-27 22:53:30 +0800

    support event emitter

---
+++ examples/chat/engine/chat.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var util = require('util');
+var events = require('events');
+
+// Private
+var messages = [];
+
+var Chat = function() {
+	var self = this;
+};
+
+util.inherits(Chat, events.EventEmitter);
+
+Chat.prototype.addMessage = function(user, msg, callback) {
+	var self = this;
+
+	var message = {
+		user: user,
+		msg: msg,
+		ts: new Date().getTime()
+	};
+
+	messages.push(message);
+
+	self.emit('message', message);
+
+	if (callback)
+		callback(null);
+};
+
+module.exports = {
+	type: 'engine',
+	engine_name: 'Chat',
+	instance: new Chat
+};
---
+++ examples/chat/public/js/index.js
@@ -0,0 +1,42 @@
+
+// Example
+App.require('Chat', function() {
+	var chat = App.Engine('Chat');
+
+	var usernameLayout = document.getElementById('username_layout');
+	var username = document.getElementById('username');
+	var sayLayout = document.getElementById('say_layout');
+	var say = document.getElementById('say');
+
+	username.onkeypress = function(e) {
+		if (e.keyCode != 13)
+			return;
+
+		usernameLayout.style.display = 'none';
+		sayLayout.style.display = '';
+
+		say.focus();
+	}
+
+	say.onkeypress = function(e) {
+
+		if (e.keyCode != 13)
+			return;
+
+		chat.addMessage(username.value, say.value, function() {
+			// Sent
+		});
+
+		// Clear input box
+		say.value = '';
+	}
+
+	chat.on('message', function(msg) {
+		var messages = document.getElementById('messages');
+		var message = document.createElement('div');
+
+		message.innerText = msg.user + ':' + msg.msg;
+
+		messages.appendChild(message);
+	});
+});
